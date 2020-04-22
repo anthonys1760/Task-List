ce8b014dcee672a9e858bf572dbffd6526646b5a
Anthony <anthony.smith1760@gmail.com>
2020-04-22 12:43:26 -0400

    Delete & Filter Tasks

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 //Define UI Variables
 const form = document.querySelector('#task-form');
 const taskList = document.querySelector('.collection');
-const clearBtn = document.querySelector('clear-tasks');
+const clearBtn = document.querySelector('.clear-tasks');
 const filter = document.querySelector('#filter');
 const taskInput = document.querySelector('#task');
 
@@ -9,8 +9,14 @@ const taskInput = document.querySelector('#task');
 loadEventListeners();
 //Load all event listeners
 function loadEventListeners() {
-//Add task event
+    //Add task event
     form.addEventListener('submit', addTask);
+    //Remove task event
+    taskList.addEventListener('click', removeTask);
+    //Clear task event
+    clearBtn.addEventListener('click', clearTasks);
+    //Filter tasks event
+    filter.addEventListener('keyup', filterTasks);
 }
 
 //Add Task
@@ -18,7 +24,6 @@ function addTask(e) {
     if (taskInput.value === '') {
         alert('Add a task');
     }
-
     //Create li element
     const li = document.createElement('li');
     //Add class
@@ -37,6 +42,38 @@ function addTask(e) {
     taskList.appendChild(li);
     //Clear input
     taskInput.value = '';
-
     e.preventDefault();
+}
+
+//Remove Task
+function removeTask(e) {
+    if (e.target.parentElement.classList.contains('delete-item')) {
+        if(confirm('Are You Sure?')) {
+        e.target.parentElement.parentElement.remove();
+        }
+    }
+}
+
+//Clear Tasks
+function clearTasks() {
+//     taskList.innerHTML = '';
+
+//Faster
+while(taskList.firstChild) {
+    taskList.removeChild(taskList.firstChild);
+}
+}
+
+function filterTasks(e) {
+    const text = e.target.value.toLowerCase();
+
+    document.querySelectorAll('.collection-item').forEach(function(task) {
+        const item = task.firstChild.textContent;
+        if (item.toLowerCase().indexOf(text) != -1) {
+            task.style.display = 'block';
+        } else {
+            task.style.display = 'none';
+        }
+    });
+
 }
